fb853c72a10b1d8166742d0801e873a7a54ec5af
darkmusic1516 <darkmusic1516@gmail.com>
2020-04-07 20:26:03 +0530

    3.8

--- insert.php
+++ insert.php
@@ -12,13 +12,11 @@ $conn =  new mysqli($host,$dbusername,$dbpassword,$dbname);
 if (!$conn) {
     die("Connection failed: " . $conn->connect_error());
 }
-echo "Connected successfully";
-
 
 $sql = "INSERT INTO code_bot(email,password) values('$username','$password')";
 if ($conn->query($sql)==true)
 {
-echo "data done";
+     include 'main.html';
 }
 $conn->close();
 ?>
